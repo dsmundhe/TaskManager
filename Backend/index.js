38dc3f16c088a13f40850a83f05889e8d8d126b4
dsmundhe <dipakmundhe2026@gmail.com>
2024-11-18 21:51:26 -0800

    dotenv setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,11 @@
-const express=require('express');
-const app=express();
-const PORT=4000;
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = 4000 || process.env.PORT;
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`)
 })
