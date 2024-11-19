ed24af314938b0c09d85cf7a4d5b7fbcd0ca43ad
dsmundhe <dipakmundhe2026@gmail.com>
2024-11-18 23:22:46 -0800

    background change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@
       <div className='parent'>
       <div className='child'>
        {/* <img src="https://i.pinimg.com/originals/2f/7f/63/2f7f63bf7a148b1de4d31565d94b83e3.gif" alt="Animaton" /> */}
-       <img src="https://i.pinimg.com/originals/54/6f/a0/546fa02d31c3759c54314aec40c76a7c.gif" alt="Animaton" />
+       {/* <img src="https://i.pinimg.com/originals/54/6f/a0/546fa02d31c3759c54314aec40c76a7c.gif" alt="Animaton" /> */}
        <h1>Hello Guys......</h1>
        <h2>Let's Start..........</h2>
      </div>
